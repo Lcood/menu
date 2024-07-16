6c04edc97643b0a323d864e6e82dc6682ced8d97
Lcood <fatumbd@protonmail.com>
2024-07-16 22:42:24 +0200

    map() return displayMenu

---
+++ index.js
@@ -0,0 +1,97 @@
+const menu = [
+  {
+    id: 1,
+    title: 'pancakes au miel',
+    category: 'petit-déjeuner',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates! `,
+  },
+  {
+    id: 2,
+    title: 'double steack',
+    category: 'dejeuner',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates! `,
+  },
+  {
+    id: 3,
+    title: ' milkshake godzilla',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates!`,
+  },
+  {
+    id: 4,
+    title: 'toast aux oeufs',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates! `,
+  },
+  {
+    id: 5,
+    title: 'Burger aux oeufs',
+    category: 'dejeuner',
+    price: 22.99,
+    img: './images/item-5.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates!`,
+  },
+  {
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img: './images/item-6.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates!`,
+  },
+  {
+    id: 7,
+    title: 'supplément bacon ',
+    category: 'petit-dejeuner',
+    price: 8.99,
+    img: './images/item-7.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates! `,
+  },
+  {
+    id: 8,
+    title: 'le classique',
+    category: 'dejeuner',
+    price: 12.99,
+    img: './images/item-8.jpeg',
+    desc: `oLorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates! `,
+  },
+  {
+    id: 9,
+    title: 'milshakes pour deux',
+    category: 'shakes',
+    price: 16.99,
+    img: './images/item-9.jpeg',
+    desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, qui magni soluta similique voluptas voluptates!`,
+  },
+];
+
+const sectionParent = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function () {
+  let displayMenu = menu.map(function (item) {
+    return `
+    <article class="menu-item">
+      <img src=${item.img} alt="menu item" class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}€</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>
+    `;
+  });
+  displayMenu = displayMenu.join('');
+  sectionParent.innerHTML = displayMenu;
+});
